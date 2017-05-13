af18d3010dfdd8a23c83816da4778f116c411a59
Mich Van Mooter <mich@vanmooter.be>
2017-05-13 20:17:30 +0200

    add filter: time

--- chat/src/main.js
+++ chat/src/main.js
@@ -4,8 +4,11 @@ import Vue from 'vue';
 import App from './App';
 import router from './router';
 import store from './store';
+import filters from './filters';
 
 Vue.config.productionTip = false;
+
+Vue.filter('time', filters.time);
 
 /* eslint-disable no-new */
 new Vue({
