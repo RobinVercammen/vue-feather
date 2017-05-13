18507ff136a0254acf1965de24de65e1bf413a2f
Mich Van Mooter <mich@vanmooter.be>
2017-05-13 18:56:19 +0200

    fetch messages on app start

--- chat/src/components/chat.vue
+++ chat/src/components/chat.vue
@@ -1,36 +1,34 @@
 <template>
-    <div>
-        <form v-on:submit.prevent="submit">
-            <div v-for="m in messages">
-                <p>{{m.text}}</p>
-                <br>
-            </div>
-            <input type="text" name="text" v-model="message" />
-            <button>Zender</button>
-        </form>
-    </div>
+    <form v-on:submit.prevent="submit">
+        <ul>
+            <li class="message" v-for="message in messages">
+                {{ message.text }}
+            </li>
+        </ul>
+        <input type="text" v-model="message" />
+        <button type="submit">Send</button>
+    </form>
 </template>
+
 <script>
-import { mapState } from 'vuex';
 export default {
     name: 'chat',
     computed: {
-        ...mapState(['messages'])
+        messages() {
+            return this.$store.state.messages;
+        }
     },
     data() {
         return {
             message: '',
         }
     },
-    created() {
-        // commit vs dispatch
-        this.$store.dispatch('getMessagesAsync');
-    },
     methods: {
-        submit(ev) {
-            const text = this.message;
-            const date = new Date();
-            this.$store.dispatch('addMessageAsync', { text, date });
+        submit() {
+            this.$store.dispatch('addMessageAsync', {
+                text: this.message,
+                date: new Date()
+            });
         }
     }
 }
